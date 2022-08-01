b8e987b7679070227609aab67c48b977d213cad3
Veer Sandhu <sandhu.s.veer@gmail.com>
2022-07-31 20:41:53 -0400

    load cache function

--- pages/demo.py
+++ pages/demo.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import tensorflow as tf
+
+@st.cache
+def load_models():
+    age_model = tf.keras.models.load_model('../models/age_model.h5')
+    ethnicity_model = tf.keras.models.load_model('../models/ethnicity_model.h5')
+    gender_model = tf.keras.models.load_model('../models/gender_model.h5')
+
 
 st.title('App Demo')
