9852f1a4935c3720804ff308e2cdd3bc95d13620
Veer Sandhu <sandhu.s.veer@gmail.com>
2022-08-05 23:39:59 -0400

    single

--- pages/demo.py
+++ pages/demo.py
@@ -7,6 +7,7 @@ def load_models():
     ethnicity_model = tf.keras.models.load_model('../models/ethnicity_model.h5')
     gender_model = tf.keras.models.load_model('../models/gender_model.h5')
 
-def predict(model_type, img_data):
-    map = []
+def predict_single(model_type, img_data):
+    map = ['age', 'ethnicity', 'gender']
+
 st.title('App Demo')
