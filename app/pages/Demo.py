1fbbc392c63cbbb7321589f5d1c215582f8e24e0
Veer Sandhu <sandhu.s.veer@gmail.com>
2022-08-15 14:29:38 -0400

    added select img from dataset into app

--- app/pages/Demo.py
+++ app/pages/Demo.py
@@ -118,7 +118,7 @@ else:
 
             col1, col2 = st.columns([1,1])
             with col1:
-                st.image(image_dir)
+                st.image(image_dir, width=300)
             with col2:
                 st.write('**Deteced...**')
 
