2276511cdbc2cec12a1865c1d3f91340c176cb1a
Veer Sandhu <sandhu.s.veer@gmail.com>
2022-08-15 13:34:44 -0400

    removed old images

--- app/pages/Demo.py
+++ app/pages/Demo.py
@@ -15,7 +15,7 @@ def load_models():
 age_model, ethnicity_model, gender_model = load_models()
 
 def predict_uploaded_image(input_image):
-    '''Process and upload an image into the 3 CNNs'''
+    '''Process and upload a local image into the 3 CNNs'''
     raw_image = Image.open(input_image).convert('RGB')
     image_pil = raw_image.resize((48,48))
 
@@ -23,21 +23,36 @@ def predict_uploaded_image(input_image):
     array1 = np.array(img1.getdata())
     processed_image = np.reshape(array1, (48,48,1)) / 255.0
 
+    return cnn_predict(processed_image)
+
+def predict_dataset_image(path):
+    '''Process and upload a dataset image into the 3 CNNs'''
+    img1 = Image.open(path).convert(mode="RGB")
+    img1 = img1.resize((48,48))
+    img1 = ImageOps.grayscale(img1)
+    array1 = np.array(img1.getdata())
+    processed_image = np.reshape(array1, (48,48,1)) / 255.0
+
+    return cnn_predict(processed_image)
+
+def cnn_predict(x_data):
+    '''Predict gender, ethnicity, and age from a 48x48 image'''
     # Gender
-    pred1 = gender_model.predict(np.expand_dims(processed_image, axis=0))[0]
+    pred1 = gender_model.predict(np.expand_dims(x_data, axis=0))[0]
     gender = 'Female'
     if pred1 < 0.5:
         gender = 'Male'
 
     # Ethnicity
-    pred2 = ethnicity_model.predict(np.expand_dims(processed_image, axis=0))
+    pred2 = ethnicity_model.predict(np.expand_dims(x_data, axis=0))
     label_map = ['Caucasian', 'African', 'East Asian', 'South Asian', 'Latino']
     ethnicity = label_map[np.argmax(pred2)]
 
     # Age
-    age = round(age_model.predict(np.expand_dims(processed_image, axis=0))[0][0])
+    age = round(age_model.predict(np.expand_dims(x_data, axis=0))[0][0])
         
     return gender, ethnicity, age
+
 # def predict_all(path):
 #     '''Predict gender, ethnicity, and age from any image'''
     
@@ -84,6 +99,26 @@ if type == 'Upload from local disk':
             col1, col2 = st.columns([1,1])
             with col1:
                 st.image(uploaded_image)
+            with col2:
+                st.write('**Deteced...**')
+
+                st.write(f'Gender: `{pred_gender}`')
+                st.write(f'Ethnicity: `{pred_ethnicity}`')
+                st.write(f'Age: `{pred_age}`')
+else:
+    image_index = st.select_slider(
+     'Select a test image',
+     options=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'])
+    
+    image_dir = 'app/demo_images/image' + image_index + '.png'
+
+    st.write(image_dir)
+    if st.button('Predict'):
+            pred_gender, pred_ethnicity, pred_age = predict_dataset_image(image_dir)
+
+            col1, col2 = st.columns([1,1])
+            with col1:
+                st.image(image_dir)
             with col2:
                 st.write('**Deteced...**')
 
