103f4de9f772b062e3fd5d49aba88a6959cde292
Veer Sandhu <sandhu.s.veer@gmail.com>
2022-08-16 23:16:57 -0400

    predict_dataset fixed

--- app/pages/1_Demo.py
+++ app/pages/1_Demo.py
@@ -26,7 +26,7 @@ def predict_uploaded_image(input_image):
     return cnn_predict(processed_image)
 
 def predict_dataset_image(path):
-    '''Process and upload a dataset image into the 3 CNNs'''
+    '''Select an image from the testing dataset and upload it into the 3 CNNs'''
     img1 = Image.open(path).convert(mode="RGB")
     img1 = img1.resize((48,48))
     img1 = ImageOps.grayscale(img1)
@@ -52,36 +52,6 @@ def cnn_predict(x_data):
     age = round(age_model.predict(np.expand_dims(x_data, axis=0))[0][0])
         
     return gender, ethnicity, age
-
-# def predict_all(path):
-#     '''Predict gender, ethnicity, and age from any image'''
-    
-#     img1 = Image.open(path).convert(mode="RGB")
-#     img1 = img1.resize((48,48))
-#     img1 = ImageOps.grayscale(img1)
-#     array1 = np.array(img1.getdata())
-#     input_img = np.reshape(array1, (48,48,1)) / 255.0
-
-#     plt.imshow(input_img)
-    
-#     # Gender
-#     pred1 = age_model.predict(np.expand_dims(input_img, axis=0))[0]
-#     gender = 'Female'
-#     if pred1 < 0.5:
-#         gender = 'Male'
-
-#     # Ethnicity
-#     pred2 = ethnicity_model.predict(np.expand_dims(input_img, axis=0))
-#     label_map = ['Caucasian', 'African', 'East Asian', 'South Asian', 'Latino']
-#     ethnicity = label_map[np.argmax(pred2)]
-
-#     # Age
-#     age = gender_model.predict(np.expand_dims(input_img, axis=0))
-        
-#     print('--Predictions--')
-#     print(f'Gender: {gender} ({pred1})')
-#     print(f'Ethnicity: {ethnicity} ({pred2[0][np.argmax(pred2)]})')
-#     print(f'Age: {age[0][0]}')
 
 st.title('App Demo')
 st.write('Using the user-inputs below, upload (or select) an **image** and run the `CNNs`')
