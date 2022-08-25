1a31a144f526c71dc56b974f4d754ec508677649
Veer Sandhu <sandhu.s.veer@gmail.com>
2022-08-24 21:56:46 -0400

    about edited

--- app/pages/2_📄_About.py
+++ app/pages/2_📄_About.py
@@ -1,8 +1,10 @@
 import streamlit as st
 
+# Page set up
 st.set_page_config(page_title="Demo", page_icon="👨", layout='centered', initial_sidebar_state="expanded")
 st.sidebar.success('Select a page above')
 
+# App
 st.title('About')
 st.write('''
 - Github Repository: `https://github.com/Real-VeerSandhu/Ethnicity-Age-Prediction`
